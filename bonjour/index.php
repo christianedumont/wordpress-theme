f20367f1bd8db87f687d3f0c8dcfc278e34021e5
Christiane Dumont <30590431+christianedumont@users.noreply.github.com>
2023-12-11 14:53:50 +0100

    Theme v1

---
+++ bonjour/index.php
@@ -0,0 +1,8 @@
+<!-- Affichage de l'entête -->
+<?php get_header(); ?>
+<!-- Affichage du main -->
+<main class="container">
+<?php get_template_part( 'template-parts/content' ); ?>
+</main>
+<!-- Affichage du footer -->
+<?php get_footer(); ?>
---
+++ bonjour/template-parts/content.php
@@ -0,0 +1,21 @@
+<?php
+// Y a t'il des contenus à afficher ?
+if ( have_posts() ) {
+	// Boucle sur les contenus
+	while ( have_posts() ) {
+		the_post(); ?>
+		<article id="post-<?php the_ID() ?>">
+			<!-- Titre de l'article -->
+			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+			<!-- Résumé de l'article -->
+			<?php the_excerpt(); ?>
+			<footer class="postmetadata">
+				<p></p>
+			</footer>
+		</article>
+	<?php }
+}
+else { ?>
+	<h2>Aucun contenu</h2>
+	<p>Désolé, mais aucun contenu n'a été trouvé ou publié.</p>
+<?php } ?>
